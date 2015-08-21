d0cabf864e63c35f8ee818d1c56727f9f1d3c1ec
Daniel Clay <dclay.daniel@gmail.com>
2015-08-21 10:13:18 +0100

    Part 5

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'snippets', 'owner')
-        
-class SnippetSerializer(serializers.ModelSerializer):
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'snippets')
